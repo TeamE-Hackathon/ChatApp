365876b0bc95bfaa8c53e548de1f150b94e9bb49
s4s7-dev <sho4.carpe.diem@gmail.com>
2022-05-21 23:22:51 +0900

    create dateTime.js as util

---
+++ api/utils/dateTime.js
@@ -0,0 +1,14 @@
+const nowTime = () => {
+    let today = new Date();
+    let year = today.getFullYear();
+    let month = today.getMonth() + 1;
+    let date = today.getDate();
+    let time = today.toTimeString();
+
+    dateTime = year + '/' + month + '/' + date + ' ' +time;
+    return dateTime
+}
+
+module.exports = {
+    nowTime
+}
