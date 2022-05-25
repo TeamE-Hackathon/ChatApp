445f467eb30973793b27340ebf84b35243fdacca
tomotakashimizu <tomotaka.developer@gmail.com>
2022-05-25 22:31:01 +0900

    clean: apply prettier to front code again

--- front/src/pages/Top.jsx
+++ front/src/pages/Top.jsx
@@ -1,56 +1,56 @@
-import React from 'react';
-import ActionAreaCard from '../components/card/Card';
-import { Grid } from '@mui/material';
-import { CreateRoomButton } from '../components/button/CreateRoomButton';
+import { Grid } from "@mui/material";
+import React from "react";
+import { CreateRoomButton } from "../components/button/CreateRoomButton";
+import ActionAreaCard from "../components/card/Card";
 
 const cardContents = [
-    {
-        title: 'Reactについて',
-        detail: 'Reactについて、なんでも話してます。',
-        imageUrl: 'https://picsum.photos/150'
-    },
-    {
-        title: 'DynamoDBについて',
-        detail: 'AWSのNoSQLのサービスであるDynamoDBについて、興味ある方はぜひ',
-        imageUrl: 'https://picsum.photos/150'
-    },
-    {
-        title: '面接について',
-        detail: '面接でどんな質問があったかメモみたいに書いてます',
-        imageUrl: 'https://picsum.photos/150'
-    },
-    {
-        title: '学び方について',
-        detail: 'みんなどんな学習法があるか気になったので部屋作りました！',
-        imageUrl: 'https://picsum.photos/150'
-    },
+  {
+    title: "Reactについて",
+    detail: "Reactについて、なんでも話してます。",
+    imageUrl: "https://picsum.photos/150",
+  },
+  {
+    title: "DynamoDBについて",
+    detail: "AWSのNoSQLのサービスであるDynamoDBについて、興味ある方はぜひ",
+    imageUrl: "https://picsum.photos/150",
+  },
+  {
+    title: "面接について",
+    detail: "面接でどんな質問があったかメモみたいに書いてます",
+    imageUrl: "https://picsum.photos/150",
+  },
+  {
+    title: "学び方について",
+    detail: "みんなどんな学習法があるか気になったので部屋作りました！",
+    imageUrl: "https://picsum.photos/150",
+  },
 ];
 
-const getCardContent = getObj => {
-    return (
-        <Grid item xs={12} sm={4}>
-            <ActionAreaCard {...getObj} />
-        </Grid>
-    );
+const getCardContent = (getObj) => {
+  return (
+    <Grid item xs={12} sm={4}>
+      <ActionAreaCard {...getObj} />
+    </Grid>
+  );
 };
 
 export const RoomsList = () => {
-    return(
-        <>
-            <Grid container direction='column'>
-                <Grid item container>
-                    <Grid sm={2} />
-                    <Grid xs={12} sm={8}>
-                        <Grid container spacing={2}>
-                            {cardContents.map(contentObj => getCardContent(contentObj))}
-                        </Grid>
-                    </Grid>
-                    <Grid sm={2} />
-                </Grid>
+  return (
+    <>
+      <Grid container direction="column">
+        <Grid item container>
+          <Grid sm={2} />
+          <Grid xs={12} sm={8}>
+            <Grid container spacing={2}>
+              {cardContents.map((contentObj) => getCardContent(contentObj))}
             </Grid>
-            <Grid container justifyContent='center'>
-                <CreateRoomButton/>
-            </Grid>
-        </>
-    );
+          </Grid>
+          <Grid sm={2} />
+        </Grid>
+      </Grid>
+      <Grid container justifyContent="center">
+        <CreateRoomButton />
+      </Grid>
+    </>
+  );
 };
