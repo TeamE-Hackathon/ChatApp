04615e7d12bbcd7f79bd0066c8363f99fdb00232
tomotakashimizu <tomotaka.developer@gmail.com>
2022-05-25 21:25:03 +0900

    update: apply prettier to front code

--- front/src/App.js
+++ front/src/App.js
@@ -1,32 +1,24 @@
-import { Link } from 'react-router-dom';
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-} from 'react-router-dom';
-
-import { Chat } from './pages/chat/Chats';
-import { NewRoom } from './pages/new_room/NewRoom';
-import { RoomsList } from './pages/Top';
-import { Login }  from './pages/login/Login';
-
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Chat } from "./pages/chat/Chats";
+import { Login } from "./pages/login/Login";
+import { NewRoom } from "./pages/new_room/NewRoom";
+import { Rooms } from "./pages/rooms/Rooms";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <nav>
-                <Link to="/">Home</Link> |{' '}
-                <Link to="/login">ログイン</Link> |{' '}
-                <Link to="/mypage">MyPage</Link> |{' '}
-            </nav>
-            <Routes>
-                <Route path="/" element={<RoomsList />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/rooms/new" element={<NewRoom />} />
-                <Route path="/rooms/:id" element={<Chat />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/login">ログイン</Link> | <Link to="/mypage">MyPage</Link> |{" "}
+        <Link to="/rooms/new">部屋作る</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Rooms />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/rooms/new" element={<NewRoom />} />
+        <Route path="/rooms/:id" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- front/src/pages/new_room/NewRoom.js
+++ front/src/pages/new_room/NewRoom.js
@@ -1,48 +1,47 @@
-import './NewRoom.css';
-import io from 'socket.io-client';
+import "./NewRoom.css";
+import io from "socket.io-client";
 
-import { useState } from 'react';
-import { Chat } from './../chat/Chats';
+import { useState } from "react";
+import { Chat } from "./../chat/Chats";
 
-const socket = io.connect('http://localhost:3001');
+const socket = io.connect("http://localhost:3001");
 
 export const NewRoom = () => {
-    const [username, setUsername] = useState('');
-    const [room, setRoom] = useState('');
-    const [showChat, setShowChat] = useState(false);
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
 
-    const joinRoom = () => {
-        if (username !== '' && room !== '') {
-            socket.emit('join_room', room);
-            setShowChat(true);
-        }
-    };
-
-    return (
-        <div className="NewRoom">
-            {!showChat ? (
-                <div className="joinChatContainer">
-                    <h3>Welcom Chat</h3>
-                    <input
-                        type="text"
-                        placeholder="名前を入力してください"
-                        onChange={(e) => {
-                            setUsername(e.target.value);
-                        }}
-                    />
-                    <input
-                        type="text"
-                        placeholder="RoomIDを入力してください"
-                        onChange={(e) => {
-                            setRoom(e.target.value);
-                        }}
-                    />
-                    <button onClick={joinRoom}>Join a Room</button>
-                </div>
-            ) : (
-                <Chat socket={socket} username={username} room={room} />
-            )}
+  return (
+    <div className="NewRoom">
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Welcom Chat</h3>
+          <input
+            type="text"
+            placeholder="名前を入力してください"
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="RoomIDを入力してください"
+            onChange={(e) => {
+              setRoom(e.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join a Room</button>
         </div>
-    );
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
+    </div>
+  );
 };
