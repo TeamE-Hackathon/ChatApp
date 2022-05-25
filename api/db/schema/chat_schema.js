bd71b6ce78124b48a930adaaa2f054afd753f26c
tomotakashimizu <tomotaka.developer@gmail.com>
2022-05-25 23:00:13 +0900

    add: .prettierrc to api

--- api/db/schema/chat_schema.js
+++ api/db/schema/chat_schema.js
@@ -47,14 +47,8 @@ const params = {
 
 dynamoDB.createTable(params, (err, data) => {
   if (err) {
-    console.error(
-      "Unable to create table. Error JSON:",
-      JSON.stringify(err, null, 2)
-    );
+    console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
   } else {
-    console.log(
-      "Created table. Table description JSON:",
-      JSON.stringify(data, null, 2)
-    );
+    console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
   }
 });
