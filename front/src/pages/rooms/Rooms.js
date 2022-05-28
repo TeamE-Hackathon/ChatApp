f21551bf53465bba2e5c97d4bce9ae9be3d39ac6
T-Y-Koga <90363397+T-Y-Koga@users.noreply.github.com>
2022-05-28 09:01:32 +0900

    add header modal button

--- front/src/pages/rooms/Rooms.js
+++ front/src/pages/rooms/Rooms.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import BasicButton from '../../components/button/BasicButton';
+// import Header from '../../components/header/Header';
+import BasicModal from '../../components/modal/BasicModal';
 
 export const Rooms = () => {
-  return <div>Topページ: ルーム一覧</div>;
+  return (
+    <div>
+      {/* <Header /> */}
+      <BasicModal />
+      <BasicButton />
+      Topページ: ルーム一覧
+    </div>
+  );
 };
