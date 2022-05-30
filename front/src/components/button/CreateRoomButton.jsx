fab5126932b00b6ecfbe9fd69a6d0faee2459224
s4s7-dev <sho4.carpe.diem@gmail.com>
2022-05-30 22:50:06 +0900

    comment out createRoomButton

--- front/src/components/button/CreateRoomButton.jsx
+++ front/src/components/button/CreateRoomButton.jsx
@@ -1,12 +1,12 @@
-import AddIcon from '@mui/icons-material/Add';
-import Button from '@mui/material/Button';
-import React from 'react';
-import { Link } from 'react-router-dom';
+// import AddIcon from '@mui/icons-material/Add';
+// import Button from '@mui/material/Button';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
 
-export const CreateRoomButton = () => {
-  return (
-    <Button variant='outlined' component={Link} to='/rooms/new' startIcon={<AddIcon />}>
-      部屋を作る
-    </Button>
-  );
-};
+// export const CreateRoomButton = () => {
+//   return (
+//     <Button variant='outlined' component={Link} to='/rooms/new' startIcon={<AddIcon />}>
+//       部屋を作る
+//     </Button>
+//   );
+// };
