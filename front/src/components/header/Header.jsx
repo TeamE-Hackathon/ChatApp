f48383cc918299bd302ae0ff19848b31ee501923
T-Y-Koga <90363397+T-Y-Koga@users.noreply.github.com>
2022-06-04 00:07:39 +0900

    update: headerのボタン種類、画面サイズ毎の表示/非表示を修正

--- front/src/components/header/Header.jsx
+++ front/src/components/header/Header.jsx
@@ -1,39 +1,54 @@
 import ForumIcon from '@mui/icons-material/Forum';
-import MenuIcon from '@mui/icons-material/Menu';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import HamburgerMenu from '../menu/HamburgerMenu';
 import './Header.css';
 
 export default function Header() {
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position='static'>
+    <Box sx={{ flexGrow: 1, borderBottom: 3, borderColor: 'blue' }}>
+      <AppBar style={{ position: 'fixed', backgroundColor: 'white' }}>
         <Toolbar>
           <div className='headerLeft'>
             <div className='headerLogo'>
-              <Link to='/'>
-                <ForumIcon />
+              <Link to='/' style={{ textDecoration: 'none', color: 'white' }}>
+                <ForumIcon style={{ fontSize: '40px', color: 'blue', marginTop: '10px' }} />
               </Link>
             </div>
             <div className='headerTitle'>
-              <Link to='/'>
-                <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+              <Link to='/' style={{ textDecoration: 'none', color: 'white' }}>
+                <Typography
+                  variant='h6'
+                  component='div'
+                  sx={{ flexGrow: 1 }}
+                  style={{ fontStyle: 'oblique', fontSize: '30px', color: 'blue' }}
+                >
                   Chat App
                 </Typography>
               </Link>
             </div>
           </div>
           <div className='headerRight'>
-            <Button color='inherit'>Login</Button>
-            <IconButton size='large' edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
-              <MenuIcon />
-            </IconButton>
+            <Link to='/signin' style={{ textDecoration: 'none', color: 'white' }}>
+              <Button variant='outlined' style={{ marginRight: '10px' }} sx={{ display: { xs: 'none', sm: 'flex' } }}>
+                My Page
+              </Button>
+            </Link>
+            <Link to='/signin' style={{ textDecoration: 'none', color: 'white' }}>
+              <Button variant='outlined' style={{ marginRight: '10px' }} sx={{ display: { xs: 'none', sm: 'flex' } }}>
+                Sign in
+              </Button>
+            </Link>
+            <Button variant='outlined' style={{ marginRight: '10px' }} sx={{ display: { xs: 'none', sm: 'flex' } }}>
+              Sign out
+            </Button>
+            <HamburgerMenu />
           </div>
         </Toolbar>
       </AppBar>
