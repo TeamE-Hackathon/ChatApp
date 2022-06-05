f5405df3e77f17f39c2c724867f382b7603fe920
s4s7-dev <sho4.carpe.diem@gmail.com>
2022-06-05 17:50:24 +0900

    update: front routing

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/header/Header';
-import { NewChat } from './pages/Chat';
-import { NewRoom } from './pages/new_room/NewRoom';
+import { Chat } from './pages/chat/Chats';
 import { SignIn } from './pages/signin/SignIn';
 import { SignUp } from './pages/signup/SignUp';
 import { RoomsList } from './pages/Top';
@@ -11,15 +10,13 @@ function App() {
     <BrowserRouter>
       <nav>
         <Header />
-        {/* <Link to='/'>Home</Link> | <Link to='/login'>ログイン</Link> | <Link to='/mypage'>MyPage</Link> |{' '}
-        <Link to='/rooms/new'>部屋作る</Link> */}
       </nav>
       <Routes>
         <Route path='/' element={<RoomsList />} />
         <Route path='/signin' element={<SignIn />} />
         <Route path='/signup' element={<SignUp />} />
-        <Route path='/rooms/new' element={<NewRoom />} />
-        <Route path='/rooms/:id' element={<NewChat />} />
+        <Route path='/rooms/:roomName' element={<Chat />} />
+        {/* <Route path='/rooms/:roomName' element={<NewChat />} /> ToDo: MUIで焼き直す */}
       </Routes>
     </BrowserRouter>
   );
