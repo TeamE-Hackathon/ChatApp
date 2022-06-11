be49d367b34ee200c3937a47c72d08534713b925
s4s7-dev <sho4.carpe.diem@gmail.com>
2022-06-11 10:06:29 +0900

    update: room list picture

--- front/src/components/card/Card.jsx
+++ front/src/components/card/Card.jsx
@@ -5,10 +5,21 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+
+const getRandomPicture = () => {
+  // TODO: 一旦、https://picsum.photos/images からいい感じの写真を取ってくる。
+  const photos = ['667/4240/2832', '/251/5184/3456', '/362/4438/2954', '/478/5184/3456'];
+  const min = Math.ceil(0);
+  const max = Math.floor(photos.length);
+  const randomIndex = Math.floor(Math.random() * (max - min) + min);
+  return photos[randomIndex];
+};
+
 export default function ActionAreaCard(props) {
   console.log('p', props);
   const { RoomName: roomName, CreatedAt: createdAt, Detail: detail } = props; // eslint-disable-line no-unused-vars
-  const imageUrl = 'https://picsum.photos/150';
+  const picturePath = getRandomPicture();
+  const imageUrl = `https://picsum.photos/id/${picturePath}`;
   return (
     <Card sx={{ maxWidth: 345 }}>
       <Link to={`/rooms/${roomName}`} state={{ roomName: roomName }} style={{ textDecoration: 'none', color: 'gray' }}>
