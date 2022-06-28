4900489ee70a102d504dccac1480f5421253a4df
rina nishi <rina-ns@leo.bbiq.jp>
2022-06-28 20:38:15 +0900

    プロフィール編集画面にアカウント情報を表示させる

--- front/src/components/avatar/MuiAvatar.jsx
+++ front/src/components/avatar/MuiAvatar.jsx
@@ -1,11 +1,15 @@
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
+import { getAuth } from 'firebase/auth';
 import PropTypes from 'prop-types';
 
 export default function ImageAvatars({ displayName }) {
+  const auth = getAuth();
+  const user = auth.currentUser;
+
   return (
     <Stack direction='row' spacing={2}>
-      <Avatar alt={displayName} src='/static/images/avatar/1.jpg' sx={{ marginTop: 20, width: 200, height: 200 }} />
+      <Avatar alt={displayName} src={user.photoURL} sx={{ marginTop: 20, width: 200, height: 200 }} />
     </Stack>
   );
 }
