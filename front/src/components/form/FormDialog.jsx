475619a69533291be4c7aa7e4d037869ca083256
rina nishi <rina-ns@leo.bbiq.jp>
2022-06-28 20:38:15 +0900

    プロフィール編集画面にアカウント情報を表示させる

--- front/src/components/form/FormDialog.jsx
+++ front/src/components/form/FormDialog.jsx
@@ -5,13 +5,62 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import TextField from '@mui/material/TextField';
+import { getAuth, onAuthStateChanged, updateEmail, updateProfile } from 'firebase/auth';
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 
 // TODO: propsとして受け取る
 // textFieldにplaceholderとしてセットする
 export default function FormDialog() {
   const [open, setOpen] = React.useState(false);
+  const [setUser] = useState('');
+  const auth = getAuth();
+  const user = auth.currentUser;
 
+  /* ↓ログインしているかどうかを判定する */
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+  }, []);
+
+  // プロフィールを更新する
+  updateProfile(auth.currentUser, {
+    displayName: 'Jane Q. User',
+    photoURL: 'https://example.com/jane-q-user/profile.jpg',
+  })
+    .then(() => {
+      // Profile updated!
+      // ...
+    })
+    .catch(() => {
+      // An error occurred
+      // ...
+    });
+
+  // emailを更新する
+  updateEmail(auth.currentUser, 'user@example.com')
+    .then(() => {
+      // Email updated!
+      // ...
+    })
+    .catch(() => {
+      // An error occurred
+      // ...
+    });
+
+  if (user !== null) {
+    const displayName = user.displayName;
+    const email = user.email;
+    const photoURL = user.photoURL;
+    const emailVerified = user.emailVerified;
+
+    // The user's ID, unique to the Firebase project. Do NOT use
+    // this value to authenticate with your backend server, if
+    // you have one. Use User.getToken() instead.
+    const uid = user.uid;
+    console.log(displayName, email, photoURL, emailVerified, uid);
+  }
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -29,35 +78,26 @@ export default function FormDialog() {
         <DialogTitle>プロフィール編集</DialogTitle>
         <DialogContent>
           <DialogContentText></DialogContentText>
-          <TextField autoFocus margin='dense' id='name' label='名前' type='name' fullWidth variant='standard' />
-          <TextField autoFocus margin='dense' id='email' label='Email' type='email' fullWidth variant='standard' />
           <TextField
-            autoFocus
             margin='dense'
-            id='password'
-            label='Password'
-            type='password'
+            id='name'
+            label='名前'
+            defaultValue={user.displayName}
+            type='name'
             fullWidth
             variant='standard'
           />
           <TextField
-            autoFocus
             margin='dense'
-            id='twitterid'
-            label='TwitterID'
-            type='id'
+            id='email'
+            label='Email'
+            defaultValue={user.email}
+            type='email'
             fullWidth
             variant='standard'
           />
-          <TextField
-            autoFocus
-            margin='dense'
-            id='skill'
-            label='技術スタック'
-            type='text'
-            fullWidth
-            variant='standard'
-          />
+          <TextField margin='dense' id='twitterid' label='TwitterID' type='id' fullWidth variant='standard' />
+          <TextField margin='dense' id='skill' label='技術スタック' type='text' fullWidth variant='standard' />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} variant='outlined'>
