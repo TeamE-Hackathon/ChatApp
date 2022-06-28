5babf865c2a882349c3cf8d90f3eee25597ab9c0
s4s7-dev <sho4.carpe.diem.0202@gmail.com>
2022-06-28 22:09:37 +0900

    update MuiAvatar.jsx

--- front/src/components/avatar/MuiAvatar.jsx
+++ front/src/components/avatar/MuiAvatar.jsx
@@ -1,19 +1,16 @@
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
-import { getAuth } from 'firebase/auth';
 import PropTypes from 'prop-types';
 
-export default function ImageAvatars({ displayName }) {
-  const auth = getAuth();
-  const user = auth.currentUser;
-
+export default function ImageAvatars({ displayName, photoURL }) {
   return (
     <Stack direction='row' spacing={2}>
-      <Avatar alt={displayName} src={user.photoURL} sx={{ marginTop: 20, width: 200, height: 200 }} />
+      <Avatar alt={displayName} src={photoURL} sx={{ marginTop: 20, width: 200, height: 200 }} />
     </Stack>
   );
 }
 
 ImageAvatars.propTypes = {
   displayName: PropTypes.string,
+  photoURL: PropTypes.string,
 };
